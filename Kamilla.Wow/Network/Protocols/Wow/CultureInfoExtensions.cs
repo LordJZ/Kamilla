41487a46855f4c2e362159cc7d76c8f34be2d9e7
LordJZ <a553r7fa1l3d@gmail.com>
2012-02-03 03:26:25 +0400

    Implement WowLocales and some CultureInfo extensions.

---
+++ Kamilla.Wow/Network/Protocols/Wow/CultureInfoExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Globalization;
+
+namespace Kamilla.Network.Protocols.Wow
+{
+    public static class CultureInfoExtensions
+    {
+        static SortedDictionary<string, WowLocales> s_code2locales = new SortedDictionary<string, WowLocales>()
+        {
+            { "en-US", WowLocales.enUS },
+            { "en-GB", WowLocales.enGB },
+            { "ko-KR", WowLocales.koKR },
+            { "fr-FR", WowLocales.frFR },
+            { "de-DE", WowLocales.deDE },
+            { "zh-CN", WowLocales.zhCN },
+            { "zh-TW", WowLocales.zhTW },
+            { "es-ES", WowLocales.esES },
+            { "es-MX", WowLocales.esMX },
+            { "ru-RU", WowLocales.ruRU },
+            { "pt-BR", WowLocales.ptBR },
+        };
+
+        public static bool IsWowCompatibleCulture(this CultureInfo culture)
+        {
+            if (culture == null)
+                throw new ArgumentNullException("culture");
+
+            return s_code2locales.ContainsKey(culture.Name);
+        }
+
+        public static WowLocales GetWowLocale(this CultureInfo culture)
+        {
+            if (culture == null)
+                throw new ArgumentNullException("culture");
+
+            WowLocales locale;
+            if (!s_code2locales.TryGetValue(culture.Name, out locale))
+                throw new ArgumentException("culture is not WoW-compatible.", "culture");
+
+            return locale;
+        }
+    }
+}
---
+++ Kamilla.Wow/Network/Protocols/Wow/WowLocales.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Kamilla.Network.Protocols.Wow
+{
+    public enum WowLocales
+    {
+        enUS = 0,
+        enGB = 0,
+        koKR = 1,
+        frFR = 2,
+        deDE = 3,
+        zhCN = 4,
+        zhTW = 5,
+        esES = 6,
+        esMX = 7,
+        ruRU = 8,
+        ptBR = 9, // Guessed
+    }
+}
