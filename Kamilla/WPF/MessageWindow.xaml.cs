5b5cea3889f0ae1779a2c04ffcc990d55ec669f5
LordJZ <a553r7fa1l3d@gmail.com>
2012-02-10 00:25:23 +0400

    Fix MessageWindow

--- Kamilla/WPF/MessageWindow.xaml.cs
+++ Kamilla/WPF/MessageWindow.xaml.cs
@@ -1,16 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace Kamilla.WPF
 {
@@ -19,31 +7,24 @@ namespace Kamilla.WPF
         MessageWindow()
         {
             InitializeComponent();
-
-            this.CommandBindings.AddRange(new[]
-            {
-                new CommandBinding(ApplicationCommands.Close, ApplicationCommand_CloseExecuted),
-            });
-
-            this.InputBindings.AddRange(new[]
-            {
-                new KeyBinding(ApplicationCommands.Close, Key.Escape, ModifierKeys.None),
-            });
         }
 
         public static void Show(Window parent, string title, string text)
         {
             var wnd = new MessageWindow();
+            wnd.WindowStyle = WindowStyle.None;
+            wnd.AllowsTransparency = true;
+            WindowBehavior.SetHasTitleBarMaximizeButton(wnd, false);
+            WindowBehavior.SetHasTitleBarMinimizeButton(wnd, false);
             wnd.Owner = parent;
             wnd.Icon = wnd.Icon;
             wnd.Style = parent.Style;
             wnd.Title = title;
             wnd.ui_tbMain.Text = text;
             wnd.ShowDialog();
-            wnd.Close();
         }
 
-        void ApplicationCommand_CloseExecuted(object sender, RoutedEventArgs e)
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
         }
